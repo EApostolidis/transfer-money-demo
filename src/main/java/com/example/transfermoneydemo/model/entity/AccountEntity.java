9f8a1a83acf8168076d1150b31991a1c8ab2358e
Evangelos Apostolidis <evangelos.apostolidis@fieldcode.com>
2023-03-13 10:48:04 +0200

    remove transactions from AccountEntity

--- src/main/java/com/example/transfermoneydemo/model/entity/AccountEntity.java
+++ src/main/java/com/example/transfermoneydemo/model/entity/AccountEntity.java
@@ -1,15 +1,11 @@
 package com.example.transfermoneydemo.model.entity;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -30,11 +26,5 @@ public class AccountEntity {
   private String currency;
 
   private LocalDateTime createdAt;
-
-  @OneToMany(mappedBy = "sourceAccountId", fetch = FetchType.EAGER)
-  private Set<TransactionEntity> transactionsAsSource = new HashSet<>();
-
-  @OneToMany(mappedBy = "targetAccountId", fetch = FetchType.EAGER)
-  private Set<TransactionEntity> transactionsAsTarget = new HashSet<>();
 
 }
